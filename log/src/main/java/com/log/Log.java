ef49dfd5ec5db5bf18112a59c482b55919a6b466
lara_ <lara_@PREDATOR>
2025-03-07 13:41:55 -0300

    ad

---
+++ log/src/main/java/com/log/Log.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.log;
+
+/**
+ *
+ * @author lara_
+ */
+public class Log {
+
+    public static void main(String[] args) {
+        System.out.println("Teste");
+    }
+}
